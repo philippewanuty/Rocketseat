fc2c17c456429a6de65c93336fc8acc295d0722b
Philippe Wanuty <philippewanuty@hotmail.com>
2023-09-16 10:48:49 -0300

    Finish Project Discover

--- Discover/script.js
+++ Discover/script.js
@@ -1,9 +1,25 @@
 function toggleId() {
-  var element = document.getElementById('Change');
+  var element = document.body;
 
-  if (element.classList.contains('ChangeTheme')) {
-    element.classList.remove('ChangeTheme');
+
+  //including class
+  element.classList.toggle('ChangeTheme')
+
+  // option 2
+ /* if (element.classList.contains('ChangeTheme')) {
+   element.classList.remove('ChangeTheme');
   } else {
     element.classList.add('ChangeTheme');
+  }*/
+
+  //changing img
+
+  const img = document.querySelector('#profile img')
+
+  if(element.classList.contains('ChangeTheme')){
+    img.setAttribute('src', 'assets/avatar-light.png');
+  }
+  else {
+    img.setAttribute('src', 'assets/avatar.png');
   }
 }
