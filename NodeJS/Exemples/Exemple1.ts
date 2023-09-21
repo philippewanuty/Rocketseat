9d88282b74436d349bf2ebe94ea6f718f0ca9bf1
Philippe Wanuty <philippewanuty@hotmail.com>
2023-09-21 17:42:12 -0300

    Exemple

---
+++ NodeJS/Exemples/Exemple1.ts
@@ -0,0 +1,20 @@
+interface Usuario {
+
+    name: string, 
+    email: string,
+    phone?: string
+}
+
+
+function sendMail ({email,name,phone}:Usuario){
+
+    console.log(`Hello ${name} your email is ${email} and your phone is ${phone}`)
+}
+
+sendMail({
+    email:'contact@philippewanuty.com',
+    name:'Philippe',
+    phone: '84999999999'
+
+})
+
---
+++ NodeJS/Slides/Notes.md
@@ -0,0 +1,4 @@
+<h1 style="text-align: center;">Notations</h1>
+
+<h3>What is API?</h3>
+<p></p>
