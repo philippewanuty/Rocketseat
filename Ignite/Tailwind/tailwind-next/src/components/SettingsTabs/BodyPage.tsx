13fcf1fb20f34d113e8ba58c7fc202786a47a5a3
Philippe Wanuty <philippewanuty@hotmail.com>
2023-10-06 21:31:00 -0300

    Adding complete your foto section

--- Ignite/Tailwind/tailwind-next/src/components/SettingsTabs/BodyPage.tsx
+++ Ignite/Tailwind/tailwind-next/src/components/SettingsTabs/BodyPage.tsx
@@ -1,4 +1,4 @@
-import { MailIcon, Search } from 'lucide-react';
+import { Cloud, MailIcon, Search, User } from 'lucide-react';
 import { InputControl, InputPrefix, InputRoot } from '../sidebar/mainNavegation/Input';
 
 export function BodyPage() {
@@ -55,11 +55,26 @@ export function BodyPage() {
 
         {/* Your foto */}
 
-        <div className="flex items-center justify-between border-b  border-zinc-200 pt-5 pb-5  ">
-          <label htmlFor="yourfoto" className="text-sm font-medium text-zinc-700">
+        <div className="grid grid-cols-form border-b  border-zinc-200 pt-5 pb-5  ">
+          <label htmlFor="photo" className="text-sm font-medium text-zinc-700">
             Your photo
             <span className="mt-0.5 block text-sm font-normal text-zinc-500">This will be displayed on your profile.</span>
           </label>
+          <div className="flex items-start gap-5">
+            <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+              <User className="w-8 h-8 text-violet-500" />
+            </div>
+            <label htmlFor="photo" className="flex-1 mx-2 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-200 py-4 px-6 ">
+              <div className="bg-zinc-50 flex h-16 w-16 items-center justify-center rounded-full">
+                <Cloud className="w-5 h-5 " />
+              </div>
+              <span className="font-sm font-normal text-zinc-500">
+                <span className=" text-sm font-semibold text-violet-700 ">Click to upload</span> or drag and drop
+              </span>
+              <span className=" text-xs text-zinc-500">SVG, PNG, JPG or GIF (max. 800x400px)</span>
+            </label>
+            <input type="file" className="sr-only" id="photo" />
+          </div>
         </div>
 
         {/* Role */}
